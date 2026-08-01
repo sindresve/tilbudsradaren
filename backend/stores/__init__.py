f9f5d8d375575fdecfc9ec296c5d77f9ceb06ca1
Sindre Melby-Svendsen <sindre07@outlook.com>
2026-08-02 01:42:18 +0200

    ryddet i litt kode

--- backend/stores/__init__.py
+++ backend/stores/__init__.py
@@ -7,36 +7,41 @@ from stores.europris import get_europris_url
 from stores.joker import get_joker_url
 from stores.spar import get_spar_url
 from stores.bunnpris import get_bunnpris_url
+from stores.meny import get_meny_url
 
-# Convert postalcode to unique id-codes for the current store
-def convert_postalcode(postalcode, pos, store):
-    url = f"https://www.coop.no/api/client/stores/search?language=nb-NO&query={postalcode}&chain={store}&latitude={pos.lat}&longitude={pos.lon}"
-    response = requests.get(url)
-    
+def convert_postalcode(postalcode, pos, chain):
+    url = "https://www.coop.no/api/client/stores/search"
+    params = {
+        "language": "nb-NO",
+        "query": postalcode,
+        "chain": chain,
+        "latitude": pos.lat,
+        "longitude": pos.lon,
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
     data = response.json()
 
     return data["stores"][0]["id"]
 
 def get_url(store, postalcode, pos):
-    if store == "coopExtra":
-        postal_id = convert_postalcode(postalcode, pos, "extra")
-        return get_coop_url(postal_id)
-    if store == "coopPrix":
-        postal_id = convert_postalcode(postalcode, pos, "prix")
+    if store in ("coopExtra", "coopPrix"):
+        chain = "extra" if store == "coopExtra" else "prix"
+        postal_id = convert_postalcode(postalcode, pos, chain)
         return get_coop_url(postal_id)
     elif store == "kiwi":
         return get_kiwi_url(postalcode)
+    elif store == "joker":
+        return get_joker_url(postalcode)
+    elif store == "bunnpris":
+        return get_bunnpris_url(pos)
     elif store == "rema":
         return get_rema_url()
     elif store == "meny":
-        return "https://kundeavis.meny.no/GetPDF.ashx"
+        return get_meny_url()
     elif store == "europris":
         return get_europris_url()
-    elif store == "joker":
-        return get_joker_url(postalcode)
     elif store == "spar":
         return get_spar_url()
-    elif store == "bunnpris":
-        return get_bunnpris_url(pos)
     else:
         raise ValueError(f"Ukjent butikk: {store}")
---
+++ backend/stores/meny.py
@@ -0,0 +1,2 @@
+def get_meny_url():
+    return "https://kundeavis.meny.no/GetPDF.ashx"
